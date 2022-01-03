b01e296f917fa8b1da16b3b20a82484c992af971
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-03 13:58:43 +0700

    add relationship

--- database/migrations/2022_01_03_040951_create_nasabah_table.php
+++ database/migrations/2022_01_03_040951_create_nasabah_table.php
@@ -18,6 +18,7 @@ class CreateNasabahTable extends Migration
             $table->string("name");
             $table->string("address");
             $table->timestamps();
+            $table->engine = "InnoDB";
         });
     }
 
--- database/migrations/2022_01_03_055917_create_transaksi_table.php
+++ database/migrations/2022_01_03_055917_create_transaksi_table.php
@@ -15,12 +15,19 @@ class CreateTransaksiTable extends Migration
     {
         Schema::create('transaksi', function (Blueprint $table) {
             $table->increments("id");
-            $table->integer("user_id");
+            $table->integer("user_id")->unsigned();
             $table->dateTime("transaction_date");
             $table->string("description");
             $table->enum("type", ["D", "C"]);
             $table->integer("amount");
             $table->timestamps();
+            $table->engine = "InnoDB";
+
+            $table->foreign("user_id")
+                ->references("account_id")
+                ->on("nasabah")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
         });
     }
 
