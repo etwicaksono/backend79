c1ff3318f3b62f64dbb0f1511614d1c3abd17891
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-03 13:09:18 +0700

    ADD transaksi migration

---
+++ database/migrations/2022_01_03_055917_create_transaksi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->increments("id");
+            $table->integer("user_id");
+            $table->dateTime("transaction_date");
+            $table->string("description");
+            $table->enum("type", ["D", "C"]);
+            $table->integer("amount");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
