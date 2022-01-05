edf2f44fe05393141c2fbabd401c503dbf175e1b
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-05 13:48:39 +0700

    error datatable

--- app/Http/Controllers/NasabahController.php
+++ app/Http/Controllers/NasabahController.php
@@ -5,12 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\NasabahModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Yajra\DataTables\DataTables;
 
 class NasabahController extends Controller
 {
-    public function showAllNasabah()
+    public function showAllNasabah(Request $request)
     {
-        return \response()->json(NasabahModel::orderBy("account_id", "desc")->get());
+        // return \response()->json(NasabahModel::orderBy("account_id", "desc")->get());
+        $data = NasabahModel::orderBy("account_id", "desc")->get();
+        \dd($data);
+        return DataTables::of($data)
+            ->addIndexColumn()
+            ->addColumn('action', function ($row) {
+                $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
+                return $actionBtn;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
     }
 
     public function nasabahForSelect2(Request $request)
